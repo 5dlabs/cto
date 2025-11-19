133e83f393326b65d8ee6568e07c4607cdf56bb3
GitHub Action <action@github.com>
2025-11-19 22:18:37 +0000

    🔧 Auto-format Rust code
    This commit was automatically generated by the CI pipeline
    to fix formatting issues detected by cargo fmt.
    
    [skip ci]

--- controller/src/tasks/code/templates.rs
+++ controller/src/tasks/code/templates.rs
@@ -2567,7 +2567,7 @@ impl CodeTemplateGenerator {
             "5DLabs-Blaze" => "claude/container-blaze.sh.hbs",
             "5DLabs-Cipher" => "claude/container-cipher.sh.hbs",
             "5DLabs-Cleo" => "claude/container-cleo.sh.hbs",
-            "5DLabs-Tess" => "claude/container.sh.hbs",  // Use default container for Tess
+            "5DLabs-Tess" => "claude/container.sh.hbs", // Use default container for Tess
             "5DLabs-Atlas" => "integration/container-atlas.sh.hbs",
             "5DLabs-Bolt" => "integration/container-bolt.sh.hbs",
             _ => {
